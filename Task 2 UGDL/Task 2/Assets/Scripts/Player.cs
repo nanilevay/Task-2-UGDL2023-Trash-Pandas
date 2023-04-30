65e5a985d8a0ec4dcc737de57bbec4c4b0696860
JoSilvaInfo <joanafsilva.info@gmail.com>
2023-04-30 06:43:43 +0100

    Bug fix: Lane transition

--- Task 2 UGDL/Task 2/Assets/Scripts/Player.cs
+++ Task 2 UGDL/Task 2/Assets/Scripts/Player.cs
@@ -18,6 +18,7 @@ public class Player : MonoBehaviour
     [SerializeField] private bool multipleTap;
 
     //Touch controlls
+    [SerializeField] private bool isTouching;
     private Vector2 startTouchPos;
     private Vector2 endTouchPos;
 
@@ -27,6 +28,16 @@ public class Player : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
 
         Swipe();
+
+        //FOR TESTING
+        if (Input.GetKeyUp("f"))
+        {
+            if (this.transform.position.x > limitLeft)
+            {
+                Debug.Log("Here");
+                transform.Translate(-2, 0, 0 * Time.deltaTime * slideSpeed, Space.World);
+            }
+        }
     }
 
     private void Swipe()
@@ -39,6 +50,7 @@ public class Player : MonoBehaviour
             //Gets start Pos of finger
             startTouchPos = Input.GetTouch(0).position;
             Debug.Log("Start");
+            isTouching = true;
         }
 
         //Detects End of touch
@@ -48,37 +60,42 @@ public class Player : MonoBehaviour
             endTouchPos = Input.GetTouch(0).position;
             Debug.Log("End");
 
-            //Sees what side the player was sent
-            if (endTouchPos.x < startTouchPos.x)
+            if (isTouching)
             {
-                Debug.Log("MoveR");
-                MoveRight();
-            }
-
-            else if (endTouchPos.x > startTouchPos.x)
-            {
-                Debug.Log("MoveL");
-                MoveLeft();
-            }
-
-
-            if (multipleTap)
-            {
-                //Sees if it was tapped 2 times
-                if (Input.GetTouch(0).tapCount == 2)
+                //Sees what side the player was sent
+                if (endTouchPos.x > startTouchPos.x)
                 {
-                    TapAtk();
+                    Debug.Log("MoveR");
+                    MoveRight();
                 }
+
+                if (endTouchPos.x < startTouchPos.x)
+                {
+                    Debug.Log("MoveL");
+                    MoveLeft();
+                }
+
+
+                //if (multipleTap)
+                //{
+                //    //Sees if it was tapped 2 times
+                //    if (Input.GetTouch(0).tapCount == 2)
+                //    {
+                //        TapAtk();
+                //    }
+                //}
+
+                //else
+                //{
+                //    //Sees if it was a tap (stationary = is the tap thingy)
+                //    if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Stationary)
+                //    {
+                //        TapAtk();
+                //    }
+                //}
             }
 
-            else
-            {
-                //Sees if it was a tap (stationary = is the tap thingy)
-                if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Stationary)
-                {
-                    TapAtk();
-                }
-            }
+            isTouching = false;
         }
     }
 
@@ -86,25 +103,22 @@ public class Player : MonoBehaviour
 
     private void MoveLeft()
     {
-
         if (this.transform.position.x > limitLeft)
         {
-            transform.Translate(Vector3.left * Time.deltaTime * slideSpeed, Space.World);
+            transform.Translate(-2, 0, 0 * Time.deltaTime * slideSpeed, Space.World);
         }
 
         if (enemyDetect == true)
         {
             playerAtk = true;
         }
-
-
     }
 
     private void MoveRight()
     {
-        if (this.transform.position.x > limitRight)
+        if (this.transform.position.x < limitRight)
         {
-            transform.Translate(Vector3.left * Time.deltaTime * slideSpeed, Space.World - 1);
+            transform.Translate(2, 0, 0 * Time.deltaTime * slideSpeed, Space.World - 1);
         }
 
         if (enemyDetect == true)
@@ -114,52 +128,55 @@ public class Player : MonoBehaviour
     }
 
     //Takes care of the ATK
-    private void TapAtk()
-    {
-        Debug.Log("Tap&Hold");
+    //private void TapAtk()
+    //{
+    //    Debug.Log("Tap&Hold");
 
-        //"Jump animation"
-        transform.Translate(Vector3.up * Time.deltaTime * slideSpeed, Space.World);
+    //    //"Jump animation"
+    //    transform.Translate(0, 2, 0 * Time.deltaTime * slideSpeed, Space.World);
 
-        //If the tap was made in the Left side of screen
-        if (Input.touches[0].position.x < Screen.width / 3)
-        {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                Debug.Log("MoveL");
-                MoveLeft();
-            }
-        }
+    //    //If the tap was made in the Left side of screen
+    //    if (Input.touches[0].position.x < Screen.width / 3)
+    //    {
+    //        if (Input.GetTouch(0).phase == TouchPhase.Began)
+    //        {
+    //            Debug.Log("MoveL");
+    //            MoveLeft();
+    //        }
+    //    }
 
 
-        //If the tap was made in the Right side of screen
-        if (Input.touches[0].position.x > Screen.width / 3)
-        {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                Debug.Log("MoveR");
-                MoveRight();
-            }
-        }
+    //    //If the tap was made in the Right side of screen
+    //    if (Input.touches[0].position.x > Screen.width / 3)
+    //    {
+    //        if (Input.GetTouch(0).phase == TouchPhase.Began)
+    //        {
+    //            Debug.Log("MoveR");
+    //            MoveRight();
+    //        }
+    //    }
 
-        ////If the tap was made in the middle (ifk if it'll be used)
-        //else
-        //{
-        //    //JumpAnim
-        //}
-    }
+    //    ////If the tap was made in the middle (ifk if it'll be used)
+    //    //else
+    //    //{
+    //    //    //JumpAnim
+    //    //}
+    ////}
 
     //Checks Enemy collisions using "Enemy" tag
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Enemy")
         {
-            enemyDetect = true;
-
-            if (playerAtk)
+            if (isTouching)
             {
-                Destroy(other.gameObject);
-                Debug.Log("Destroy");
+                enemyDetect = true;
+
+                if (playerAtk)
+                {
+                    Destroy(other.gameObject);
+                    Debug.Log("Destroy");
+                }
             }
 
             else
